0612f8da1462a0dedcd40c81eae0c291d7bd9281
cnnben002 <benjamin.connolly@icloud.com>
2022-05-13 13:08:14 +0200

    Updated setup

---
+++ EdgeDetection/OpenCL/Kernel.cl
@@ -0,0 +1,15 @@
+//Yoda Project
+//Group 1
+//12/05/2022
+
+__kernel void edgeDetector(__global int* imageMatrix, __global int* outputMatrix)
+{
+	//Work item and work groups numbers
+	int workItemNum = get_global_id(0);     //Work item ID
+	int workGroupNum = get_group_id(0);     //Work group ID
+	int items = get_global_size(0);         //Number of items in image
+	int rows = get_local_size(0);		//Number of rows
+	int cols = items/rows;			//Number of columns
+
+	//Code here
+}
---
+++ preprocessing.py
@@ -0,0 +1,46 @@
+#Yoda Project
+#Group 1
+#Benjamin Connolly
+#12/05/2022
+
+#Importing necessary libraries
+from PIL import Image
+from numpy import asarray
+
+#Opening the image
+image = Image.open("yoda.jpg")
+
+#Getting image properties
+format = image.format
+col, row = image.size
+
+print("Format: " + str(format))
+print("Width: " + str(col))
+print("Height: " + str(row))
+
+#Getting array of data
+data = asarray(image)
+
+print("Image conversion done...")
+
+#Saving the array to a txt file
+f = open("imageData.txt", "w")
+
+s = str(row)+"\n"
+f.write(s)
+s = str(col)+"\n"
+f.write(s)
+
+print("Writing file...")
+
+for x in range(row):
+    for y in range(col):
+        s = ""
+        for i in range(3):
+            s += str(data[x][y][i])
+            s += " "
+        s += "\n"
+        f.write(s)
+f.close()
+
+print("File saved.")
